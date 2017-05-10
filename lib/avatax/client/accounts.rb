93d2bf2095e387f1a135f4c937cdec38f65cd7ef
ted-spence-avalara <ted.spence@avalara.com>
2017-05-10 10:35:35 -0700

    Generate from final May 17.5 published swagger

--- lib/avatax/client/accounts.rb
+++ lib/avatax/client/accounts.rb
@@ -1,95 +1,108 @@
 module AvaTax
   class Client
-    module Accounts
-      # Retrieve all accounts
-      #
-      # # For Registrar Use Only
-      #
-      # @param options [Hash] with include, filter, top, skip, and order_by params
-      # @return [Hashie::Mash] The requested accounts.
-      # @example Returns a list of accounts
-      #   AvaTax.query_accounts({ $filter: '' })
-      #
-      # _You must be authenticated as an Avalara Registrar to use this route_
-      # @see http://developer.avalara.com/avatax/filtering-in-rest/ More information on filtering in REST
-      def query_accounts(options={})
-        get('/api/v2/accounts', options)
+    module Accounts 
+
+
+      # Reset this account's license key
+      # 
+      # Resets the existing license key for this account to a new key.
+      # To reset your account, you must specify the ID of the account you wish to reset and confirm the action.
+      # Resetting a license key cannot be undone. Any previous license keys will immediately cease to work when a new key is created.
+      # 
+      # @param int id The ID of the account you wish to update.
+      # @param ResetLicenseKeyModel model A request confirming that you wish to reset the license key of this account.
+      # @return LicenseKeyModel
+      def account_reset_license_key(id, model)
+        path = "/api/v2/accounts/#{id}/resetlicensekey"
+        
+        post(path, model)
       end
 
-      # Retrieve an account
-      #
-      # @param options
-      #
+
+      # Activate an account by accepting terms and conditions
+      # 
+      # Activate the account specified by the unique accountId number.
+      # 
+      # This activation request can only be called by account administrators. You must indicate 
+      # that you have read and accepted Avalara's terms and conditions to call this API.
+      # 
+      # If you have not read or accepted the terms and conditions, this API call will return the
+      # unchanged account model.
+      # 
+      # @param int id The ID of the account to activate
+      # @param ActivateAccountModel model The activation request
+      # @return AccountModel
+      def activate_account(id, model)
+        path = "/api/v2/accounts/#{id}/activate"
+        
+        post(path, model)
+      end
+
+
+      # Retrieve a single account
+      # 
+      # Get the account object identified by this URL.
+      # You may use the '$include' parameter to fetch additional nested data:
+      # 
       # * Subscriptions
-      # * Users 
-      # @return [Hashie::Mash] The requested accounts.
-      # @example Returns a list of accounts
-      #   AvaTax.get_account()
+      # * Users
+      # 
+      # @param int id The ID of the account to retrieve
+      # @param string include A comma separated list of child objects to return underneath the primary object.
+      # @return AccountModel
       def get_account(id, options={})
-        get("/api/v2/accounts/#{id}", options || {})
-      end
-
-      # Create an AvaTax account
-      #
-      # # For Registrar Use Only
-      # Create an account
-      #
-      # @param model [Hash] the account object you wish to create
-      #
-      # @return [Hashie::Mash] AccountModel
-      # @example Creates an account
-      #   AvaTax.create_account({ id: 1233, name: 'Avalara', ... })
-      def create_account(model)
-        post("/api/v2/accounts", model)
+        path = "/api/v2/accounts/#{id}"
+        
+        get(path, options)
       end
 
 
-      # Update a single account
-      #
-      # # For Registrar Use Only
-      # Delete an account. Deleting an account will delete all companies and all account level users attached to this account.
-      #
-      # @param id [Integer] the ID of the account to update
-      # @param model [Hash] the account object you wish to udpate
-      #
-      # @return [Hashie::Mash] AccountModel
-      # @example Updates an existing account
-      #   AvaTax.update_account(10, { id: 10, name: 'Avalara', ... })
-      def update_account(id, model)
-        post("/api/v2/accounts/#{id}", model)
+      # Get configuration settings for this account
+      # 
+      # Retrieve a list of all configuration settings tied to this account.
+      # 
+      # Configuration settings provide you with the ability to control features of your account and of your
+      # tax software. The category names `TaxServiceConfig` and `AddressServiceConfig` are reserved for
+      # Avalara internal software configuration values; to store your own account-level settings, please
+      # create a new category name that begins with `X-`, for example, `X-MyCustomCategory`.
+      # 
+      # Account settings are permanent settings that cannot be deleted. You can set the value of an
+      # account setting to null if desired.
+      # 
+      # Avalara-based account settings for `TaxServiceConfig` and `AddressServiceConfig` affect your account's
+      # tax calculation and address resolution, and should only be changed with care.
+      # 
+      # @param int id 
+      # @return AccountConfigurationModel[]
+      def get_account_configuration(id)
+        path = "/api/v2/accounts/#{id}/configuration"
+        
+        get(path)
       end
 
-      # Delete a single account
-      #
-      # # For Registrar Use Only
-      #
-      # @param id [Integer] the ID of the account to delete
-      #
-      # @return [Hashie:Mash] information about the account deletion
-      # @example Deletes an existing account
-      #   AvaTax.delete_account(10)
-      def delete_account(id)
-        res = delete("/api/v2/accounts/#{id}")
-        res.is_a?(Array) ? res[0] : res
-      end
 
-      # Resets the existing license key for this account to a new key. To reset your account, you must specify the ID of the account you wish to reset and confirm the action. Resetting a license key cannot be undone. Any previous license keys will immediately cease to work when a new key is created.
-      #
-      # @param options [Hash] should be a hash with { confirmResetLicenseKey: true }
-      #
-      # @return [Hashie::Mash] The result of the action
-      #
-      # @example
-      #   AvaTax.reset_license_key(4444999, { confirmLicenseKey: true })
-      #```
-      # {
-      #  "accountId": 123456789,
-      #  "privateLicenseKey": "742A02AA463CDE741E932EC365360CA3CF378BD9",
-      #  "httpRequestHeader": "Basic MTIzNDU2Nzg5Ojc0MkEwMkFBNDYzQ0RFNzQxRTkzMkVDMzY1MzYwQ0EzQ0YzNzhCRDk="
-      # }
-      #```
-      def reset_license_key(id, options={})
-        post("/api/v2/accounts/#{id}/resetlicensekey", options)
+      # Change configuration settings for this account
+      # 
+      # Update configuration settings tied to this account.
+      # 
+      # Configuration settings provide you with the ability to control features of your account and of your
+      # tax software. The category names `TaxServiceConfig` and `AddressServiceConfig` are reserved for
+      # Avalara internal software configuration values; to store your own account-level settings, please
+      # create a new category name that begins with `X-`, for example, `X-MyCustomCategory`.
+      # 
+      # Account settings are permanent settings that cannot be deleted. You can set the value of an
+      # account setting to null if desired.
+      # 
+      # Avalara-based account settings for `TaxServiceConfig` and `AddressServiceConfig` affect your account's
+      # tax calculation and address resolution, and should only be changed with care.
+      # 
+      # @param int id 
+      # @param AccountConfigurationModel[] model 
+      # @return AccountConfigurationModel[]
+      def set_account_configuration(id, model)
+        path = "/api/v2/accounts/#{id}/configuration"
+        
+        post(path, model)
       end
 
     end
