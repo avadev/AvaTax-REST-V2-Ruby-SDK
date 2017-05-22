4f97271b0cd8e85044dd12af240a5220070ecb7e
Marcus Vorwaller <marcus@vorwaller.net>
2017-05-22 11:53:40 -0700

    Update Travis file, RakeFile etc., Thanks @eric-pigeon

--- lib/avatax/client.rb
+++ lib/avatax/client.rb
@@ -3,5 +3,30 @@ module AvaTax
     Dir[File.expand_path('../client/*.rb', __FILE__)].each{|f| require f}
 
     include AvaTax::Client::Accounts
+    include AvaTax::Client::Addresses
+    include AvaTax::Client::Batches
+    include AvaTax::Client::Companies
+    include AvaTax::Client::Contacts
+    include AvaTax::Client::Definitions
+    include AvaTax::Client::FilingCalendars
+    include AvaTax::Client::Filings
+    include AvaTax::Client::Free
+    include AvaTax::Client::FundingRequests
+    include AvaTax::Client::Items
+    include AvaTax::Client::JurisdictionOverrides
+    include AvaTax::Client::Locations
+    include AvaTax::Client::Nexus
+    include AvaTax::Client::Notices
+    include AvaTax::Client::Onboarding
+    include AvaTax::Client::PointOfSale
+    include AvaTax::Client::Registrar
+    include AvaTax::Client::Settings
+    include AvaTax::Client::Subscriptions
+    include AvaTax::Client::TaxCodes
+    include AvaTax::Client::TaxRules
+    include AvaTax::Client::Transactions
+    include AvaTax::Client::Upcs
+    include AvaTax::Client::Users
+    include AvaTax::Client::Utilities
   end
 end
