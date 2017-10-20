030b2f97fcb8564803fb3f652ad71c434b1f3edf
Marcus Vorwaller <marcus@vorwaller.net>
2017-10-20 11:51:57 -0700

    Update test, gitignore

--- spec/avatax/client/transactions_spec.rb
+++ spec/avatax/client/transactions_spec.rb
@@ -1,31 +1,40 @@
 require File.expand_path('../../../spec_helper', __FILE__)
 
 describe AvaTax::Client do
-  before do
-    @client = AvaTax::Client.new()
-  end
 
   describe ".transactions" do
-    it "should create a transaction" do
-      createTransactionModel = Object.new
-      createTransactionModel.type = 'SalesInvoice'
-      createTransactionModel.companyCode = 'DEFAULT'
-      createTransactionModel.date = '2017-06-15'
-      createTransactionModel.customerCode = 'ABC'
-      createTransactionModel.addresses = Array.new
-      createTransactionModel.addresses[0].SingleLocation = Object.new
-      createTransactionModel.addresses[0].SingleLocation.line1 = '2000 Main Street'
-      createTransactionModel.addresses[0].SingleLocation.city = 'Irvine'
-      createTransactionModel.addresses[0].SingleLocation.region = 'CA'
-      createTransactionModel.addresses[0].SingleLocation.country = 'US'
-      createTransactionModel.addresses[0].SingleLocation.postalCode = '92614'
-      createTransactionModel.lines = Array.new
-      createTransactionModel.lines[0].amount = 100
-      
-      transaction = @client.create_transaction(createTransactionModel, null)
-      expect(transaction).to be_a Object
-      expect(transaction['code']).to be_a String
-      expect(transaction['tax']).to gt 0.0
+    before do
+      @base_transaction = {
+        type: 'SalesInvoice',
+        companyCode: @company_code,
+        date: '2017-06-05',
+        customerCode: 'ABC',
+        addresses: {
+          ShipFrom: {
+            line1: "123 Main Street",
+            city: "Irvine",
+            region: "CA",
+            country: "US",
+            postalCode: "92615"
+          },
+          ShipTo: {
+            line1: "100 Market Street",
+            city: "San Francisco",
+            region: "CA",
+            country: "US",
+            postalCode: "94105"
+          }
+        },
+        lines: [{amount: 100}]
+      }
     end
+
+    it "should create a transaction" do
+      transaction = @client.create_transaction(@base_transaction)
+      expect(transaction).to be_a Object
+      expect(transaction["id"]).to be_a Integer
+      expect(transaction["status"]).to eq "Saved"
+    end
+
   end
 end
