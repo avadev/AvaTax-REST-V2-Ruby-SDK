1ecaac3d456f75fb5d0aa6844cff98eaeb9e03e7
Genevieve Conty <contygm@gmail.com>
2019-02-05 11:44:52 -0800

    v19.1.1 update

--- lib/avatax/client/filings.rb
+++ lib/avatax/client/filings.rb
@@ -349,6 +349,24 @@ module AvaTax
       end
 
 
+      # Retrieve a list of filings for the specified company in the year and month of a given filing period.
+      #
+      # This API is available by invitation only.
+      # A "filing period" is the year and month of the date of the latest customer transaction allowed to be reported on a filing,
+      # based on filing frequency of filing.
+      # @param companyId [Integer] The ID of the company that owns the filings.
+      # @param year [Integer] The year of the filing period.
+      # @param month [Integer] The two digit month of the filing period.
+      # @param country [String] The two-character ISO-3166 code for the country.
+      # @param region [String] The two or three character region code for the region.
+      # @param formCode [String] The unique code of the form.
+      # @return [FetchResult]
+      def get_tax_filings(companyId, options={})
+        path = "/api/v2/companies/#{companyId}/filings"
+        get(path, options)
+      end
+
+
       # Rebuild a set of filings for the specified company in the given filing period.
       #
       # This API is available by invitation only.
