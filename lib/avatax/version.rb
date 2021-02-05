c2ef43698f5e8c250ee7adbe3ce8a7a12675e306
shilpa khanal <shilpa.khanal@avalara.com>
2021-02-05 20:24:39 +0000

    Revert " update"
    This reverts commit f233e99e42e3f474317314ad17bc82c8203c5d22, reversing
    changes made to eed2a4aff8c92502d6683241ea302b4736564354.

--- lib/avatax/version.rb
+++ lib/avatax/version.rb
@@ -1,7 +1,3 @@
 ﻿module AvaTax
-<<<<<<< HEAD
   VERSION = '21.1.1'.freeze unless defined?(::AvaTax::VERSION)
-=======
-  VERSION = '21.1.1'.freeze unless defined?(::AvaTax::VERSION)
->>>>>>> ec31911d922a2f4b66e69fa4596b378361e4c675
 end
